256cb4e518f7330cfacc49f3d57850113e998b45
Genos15 <cynapsenjoh@gmail.com>
2022-04-23 00:39:58 +0100

    configured kotlin for docker-compose

--- services/file/src/main/kotlin/com/thintwice/archive/file/FileMicroserviceApplication.kt
+++ services/file/src/main/kotlin/com/thintwice/archive/file/FileMicroserviceApplication.kt
@@ -2,8 +2,10 @@ package com.thintwice.archive.file
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
+@RestController
 class FileMicroserviceApplication
 
 fun main(args: Array<String>) {
